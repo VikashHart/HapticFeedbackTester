3fcf7dbd4ef0ebf764272a34f5514e043fbde98b
Vikash Hart <vikashhart@gmail.com>
2020-05-26 13:14:44 -0400

    Initial Commit

---
+++ HapticFeedbackTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticFeedbackTester
+//
+//  Created by Vikash Hart on 5/26/20.
+//  Copyright © 2020 Vikash Hart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
